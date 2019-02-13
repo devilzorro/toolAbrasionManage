954a50cc2c0b99246adf6b601d1a3edf1fb5f9ad
Gao Zhelong <gaozhelong@isesol.com>
2019-02-13 16:46:13 +0800

    windows mqtt test

--- main.cpp
+++ main.cpp
@@ -9,8 +9,9 @@
 #include "json/json.h"
 
 #ifdef WIN32
-#include "mqtt/async_client.h"
-//#include "redisClient/redisClient.h"
+#include "mqtt-win/async_client.h"
+#include "redisClient/redisClient.h"
+#pragma comment(lib,"ws2_32.lib")
 #else
 #include <mqtt/async_client.h>
 #include "redisClient/redisClient.h"
@@ -27,49 +28,49 @@ const string CLIENT_ID {"TESTXXX"};
 const string SERVER_ADDRESS {"testxxxxxx"};
 
 const int N_RETRY_ATTEMPTS = 5;
-const int QOS = 1;
+//const int QOS = 1;
 
 static bool mqttStatus = false;
 
-//class callback : public virtual mqtt::callback {
-//public:
-//
-//    /**
-//	 * This method is called when the client is connected.
-//	 * @param cause
-//	 */
-//	bool bConnStatus = false;
-//
-//    virtual void connected(const string &cause) {
-//        cout << "callback->connected..." << cause << endl;
-////        DEBUGLOG(cause);
-//        bConnStatus = true;
-//        int ret;
-//    }
-//
-//    /**
-//     * This method is called when the connection to the server is lost.
-//     * @param cause
-//     */
-//    virtual void connection_lost(const string &cause) {
-//        cout << "callback->connection_lost..." << cause << endl;
-////        DEBUGLOG(cause);
-////        mqtt_connect_flag = false;
-//        //...
-//        bConnStatus = false;
-//    }
-//
-//    void delivery_complete(mqtt::delivery_token_ptr tok) override {
-//        cout << "\tDelivery complete for token: "
-//             << (tok ? tok->get_message_id() : -1) << endl;
-//    }
-//
-//    void message_arrived(mqtt::const_message_ptr msg) override {
-//        std::cout << "Message arrived :" << std::endl;
-//        std::cout << "\ttopic: '" << msg->get_topic() << "'" << std::endl;
-//        std::cout << "\tpayload: '" << msg->to_string() << "'\n" << std::endl;
-//    }
-//};
+class callback : public virtual mqtt::callback {
+public:
+
+    /**
+	 * This method is called when the client is connected.
+	 * @param cause
+	 */
+	bool bConnStatus = false;
+
+    virtual void connected(const string &cause) {
+        cout << "callback->connected..." << cause << endl;
+//        DEBUGLOG(cause);
+        bConnStatus = true;
+        int ret;
+    }
+
+    /**
+     * This method is called when the connection to the server is lost.
+     * @param cause
+     */
+    virtual void connection_lost(const string &cause) {
+        cout << "callback->connection_lost..." << cause << endl;
+//        DEBUGLOG(cause);
+//        mqtt_connect_flag = false;
+        //...
+        bConnStatus = false;
+    }
+
+    void delivery_complete(mqtt::delivery_token_ptr tok) override {
+        cout << "\tDelivery complete for token: "
+             << (tok ? tok->get_message_id() : -1) << endl;
+    }
+
+    void message_arrived(mqtt::const_message_ptr msg) override {
+        std::cout << "Message arrived :" << std::endl;
+        std::cout << "\ttopic: '" << msg->get_topic() << "'" << std::endl;
+        std::cout << "\tpayload: '" << msg->to_string() << "'\n" << std::endl;
+    }
+};
 
 
 void processMsg(string passData) {
@@ -91,32 +92,36 @@ void initConfigFile(string filePth) {
 }
 
 int main(int argc,char *argv[]) {
-    string strJson = "{\"uploadid\": \"UP000000\",\"code\": 100,\"msg\": \"\",\"files\": \"\"}";
-    Json::Reader reader;
-    Json::Value root;
-    if (reader.parse(strJson,root)) {
-        string id = root["uploadid"].asString();
-        cout<<"json reader:"<<id;
-    } else {
-        return 1;
-    }
+//    string strJson = "{\"uploadid\": \"UP000000\",\"code\": 100,\"msg\": \"\",\"files\": \"\"}";
+//    Json::Reader reader;
+//    Json::Value root;
+//    if (reader.parse(strJson,root)) {
+//        string id = root["uploadid"].asString();
+//        cout<<"json reader:"<<id;
+//    } else {
+//        return 1;
+//    }
 
-//    CRedisClient redisClient;
+    CRedisClient redisClient;
 
     //子线程初始化
-    string strTest = "hello world!";
-    thread tProcessMsg(processMsg,strTest);
-    tProcessMsg.detach();
+//    string strTest = "hello world!";
+//    thread tProcessMsg(processMsg,strTest);
+//    tProcessMsg.detach();
 
 
     //mqtt broker 连接
-//    mqtt::async_client client("tcp://localhost:1883","test1");
-//    mqtt::connect_options opts;
-//    opts.set_keep_alive_interval(20);
-//    opts.set_clean_session(true);
-//    callback cb;
-//    client.set_callback(cb);
-//
+    mqtt::async_client client("tcp://0.0.0.0:61613","test1");
+    mqtt::connect_options opts;
+//    mqtt::string_ref usrname
+    opts.set_keep_alive_interval(20);
+    opts.set_clean_session(true);
+//    opts.set_user_name("admin");
+//    opts.set_password("password");
+    callback cb;
+    client.set_callback(cb);
+    cout<<"mqtt init finish********"<<endl;
+
 
 //    try {
 //        cout << "\nConnecting..." << endl;
@@ -140,11 +145,11 @@ int main(int argc,char *argv[]) {
 //        cerr << exc.what() << endl;
 //        return 1;
 //    }
-
-    cout<<"input q to quit"<<endl;
-    while (std::tolower(std::cin.get()) != 'x') {
-    }
-
+//
+//    cout<<"input q to quit"<<endl;
+////    while (std::tolower(std::cin.get()) != 'x') {
+////    }
+//
     while (std::tolower(std::cin.get()) != 'q');
 
 //    try {
