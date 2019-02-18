3b3af0939c3eb1ef8f0bbd8349bca1102375027f
Gao Zhelong <gaozhelong@isesol.com>
2019-02-18 18:25:23 +0800

    windows tool dll test

--- main.cpp
+++ main.cpp
@@ -6,12 +6,14 @@
 #include <thread>
 #include <chrono>
 #include <atomic>
+#include <fstream>
 #include "json/json.h"
 #include "redisClient/redisClient.h"
 
 #ifdef WIN32
 #include "mqtt-win/async_client.h"
 #include <Windows.h>
+#include "win_stdafx/stdafx.h"
 #pragma comment(lib,"ws2_32.lib")
 #else
 #include <mqtt/async_client.h>
@@ -20,18 +22,14 @@
 
 using namespace std;
 
-const string LOCALADDR {"tcp://localhost:1883"};
+struct Results {
+    double *r;
+    int n;
+};
 
-const string TOPIC {"hello"};
-
-const string CLIENT_ID {"TESTXXX"};
-
-const string SERVER_ADDRESS {"testxxxxxx"};
-
-const int N_RETRY_ATTEMPTS = 5;
-//const int QOS = 1;
-
-static bool mqttStatus = false;
+typedef double *(*pInitFun)();
+typedef int (*pFeedFun)(int toolNum, double load,double *p);
+typedef Results (*pResultFun)(double *p);
 
 class callback : public virtual mqtt::callback {
 public:
@@ -93,85 +91,134 @@ void initConfigFile(string filePth) {
 }
 
 int main(int argc,char *argv[]) {
-    string strJson = "{\"uploadid\": \"UP000000\",\"code\": 100,\"msg\": \"\",\"files\": \"\"}";
-    Json::Reader reader;
-    Json::Value root;
-    if (reader.parse(strJson,root)) {
-        string id = root["uploadid"].asString();
-        cout<<"json reader:"<<id;
-    } else {
-        return 1;
+    pInitFun initFun;
+    pFeedFun feedFun;
+    pResultFun resultFun;
+#ifdef WIN32
+    HINSTANCE winDll = LoadLibrary(_T("tooldll.dll"));
+    if (winDll != NULL) {
+        initFun = (pInitFun)GetProcAddress(winDll,"initial");
+        feedFun = (pFeedFun)GetProcAddress(winDll,"feed");
+        resultFun = (pResultFun)GetProcAddress(winDll,"result");
+    }
+#else
+#endif
+
+    double *p;
+    double *r;
+    int n;
+    Results re;
+    double temp;
+    double toolnum;
+    double load;
+
+    ifstream f;
+    f.open("rawdata.txt");
+
+    p = initFun();
+    for (int i = 0; i < 4000; i++) {
+        for (int j = 0; j < 16; j++) {
+            f >> temp;
+            cout << "temp content:" << temp;
+            if (j == 2) {
+                toolnum = temp;
+            }
+            else if (j == 3)
+            {
+                load = temp;
+            }
+        }
+        cout << endl;
+
+        feedFun(toolnum,load,p);
+    }
+    f.close();
+
+    re = resultFun(p);
+    printf("******win tooldll run success*******\n");
+    r = re.r;
+    n = re.n;
+    for (int i = 0; i < n; ++i) {
+        cout<<"toolNUm:"<<r[i*2]<<endl;
+        cout<<"resultValue:"<<r[i*2+1]<<endl;
     }
 
-    CRedisClient redisClient;
+//    string strJson = "{\"uploadid\": \"UP000000\",\"code\": 100,\"msg\": \"\",\"files\": \"\"}";
+//    Json::Reader reader;
+//    Json::Value root;
+//    if (reader.parse(strJson,root)) {
+//        string id = root["uploadid"].asString();
+//        cout<<"json reader:"<<id;
+//    } else {
+//        return 1;
+//    }
+
+//    CRedisClient redisClient;
 
     //子线程初始化
-    string strTest = "hello world!";
-    thread tProcessMsg(processMsg,strTest);
-    tProcessMsg.detach();
+//    string strTest = "hello world!";
+//    thread tProcessMsg(processMsg,strTest);
+//    tProcessMsg.detach();
 
 
     //mqtt broker 连接
-    mqtt::async_client client("tcp://localhost:1883","test1");
-    mqtt::connect_options opts;
-//    mqtt::string_ref usrname
-    opts.set_keep_alive_interval(20);
-    opts.set_clean_session(true);
-//    opts.set_user_name("admin");
-//    opts.set_password("password");
-    callback cb;
-    client.set_callback(cb);
-    cout<<"mqtt init finish********"<<endl;
+//    mqtt::async_client client("tcp://localhost:1883","test1");
+//    mqtt::connect_options opts;
+//    opts.set_keep_alive_interval(20);
+//    opts.set_clean_session(true);
+//    callback cb;
+//    client.set_callback(cb);
+//    cout<<"mqtt init finish********"<<endl;
 
 
-   try {
-       cout << "\nConnecting..." << endl;
-       mqtt::token_ptr conntok = client.connect(opts);
-       cout << "Waiting for the connection..." << endl;
-       conntok->wait();
-       client.start_consuming();
-       client.subscribe("hello",1)->wait();
-       cout<<"subscr topic hello ok!"<<endl;
-
-
-       cout << "  ...OK" << endl;
-
-       cout<<"callback member conn status:"<<cb.bConnStatus<<endl;
-
-       mqtt::message_ptr msg = mqtt::make_message("hello","msg content1");
-       msg->set_qos(0);
-       client.publish(msg)->wait_for(10);
-       cout<<"pub msg ok"<<endl;
-   } catch (const mqtt::exception &exc) {
-       cerr << exc.what() << endl;
-       return 1;
-   }
+//   try {
+//       cout << "\nConnecting..." << endl;
+//       mqtt::token_ptr conntok = client.connect(opts);
+//       cout << "Waiting for the connection..." << endl;
+//       conntok->wait();
+//       client.start_consuming();
+//       client.subscribe("hello",1)->wait();
+//       cout<<"subscr topic hello ok!"<<endl;
+//
+//
+//       cout << "  ...OK" << endl;
+//
+//       cout<<"callback member conn status:"<<cb.bConnStatus<<endl;
+//
+//       mqtt::message_ptr msg = mqtt::make_message("hello","msg content1");
+//       msg->set_qos(0);
+//       client.publish(msg)->wait_for(10);
+//       cout<<"pub msg ok"<<endl;
+//   } catch (const mqtt::exception &exc) {
+//       cerr << exc.what() << endl;
+//       return 1;
+//   }
 //
 //    cout<<"input q to quit"<<endl;
 ////    while (std::tolower(std::cin.get()) != 'x') {
 ////    }
 //
+    cout<<"input q to exit"<<endl;
     while (std::tolower(std::cin.get()) != 'q') {
-        bool redisStatus = redisClient.Connect("127.0.0.1",6379);
-        cout<<"redisConn status:"<<redisStatus<<endl;
-        cout<<"redis connetct status:"<<redisClient.CheckStatus()<<endl;
+//        bool redisStatus = redisClient.Connect("127.0.0.1",6379);
+//        cout<<"redisConn status:"<<redisStatus<<endl;
+//        cout<<"redis connetct status:"<<redisClient.CheckStatus()<<endl;
     }
 
-   try {
-       client.unsubscribe("hello")->wait();
-       client.stop_consuming();
-       client.disconnect()->wait();
-       cout<<"discontent ok"<<endl;
-   } catch (mqtt::exception &exc) {
-       cerr << exc.what() << endl;
-       return 1;
-   }
-    try {
-        redisClient.Disconnect();
-    }catch (exception e) {
-        cerr << e.what()<<endl;
-    }
-
+//   try {
+//       client.unsubscribe("hello")->wait();
+//       client.stop_consuming();
+//       client.disconnect()->wait();
+//       cout<<"discontent ok"<<endl;
+//   } catch (mqtt::exception &exc) {
+//       cerr << exc.what() << endl;
+//       return 1;
+//   }
+//    try {
+//        redisClient.Disconnect();
+//    }catch (exception e) {
+//        cerr << e.what()<<endl;
+//    }
 
     return 0;
 }
